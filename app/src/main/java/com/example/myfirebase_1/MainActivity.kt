e77523992855b3a9641179437ac00b1db337f6e1
Ahmed_Awwad <80257246+ahmedawwad407@users.noreply.github.com>
2023-02-27 10:27:14 +0200

    test

---
+++ app/src/main/java/com/example/myfirebase_1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myfirebase_1
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
